91134b57b9c6d1aee09081d491fac0e13a7190a5
cg2984 <31659760+cg2984@users.noreply.github.com>
2018-03-01 18:19:38 -0500

    Add files via upload

---
+++ LumaLegacy_V2/LumaLegacy_V2.ino
@@ -0,0 +1,43 @@
+int switchPin = 2; 
+int switchState; 
+unsigned long curMil; 
+unsigned long prevMil = 0; 
+int ledPin = 13;
+int seconds = 3000; 
+
+void setup() {
+  Serial.begin(9600); 
+  pinMode(ledPin, OUTPUT);
+  pinMode(switchPin, INPUT);   
+}
+
+void loop() {
+  //tells me if the button is on or off
+  switchState = digitalRead(switchPin); 
+  Serial.println(switchState); 
+  //if the button is on, and if three seconds has passed, then light the LED
+  if (switchState == 0){
+    curMil = millis();//holds the current milliseconds
+    Serial.println(curMil);//prints the current milliseconds
+    if ((unsigned long)(curMil-prevMil > 3000 && switchState == 0)){//says if the current time minus the last time i made an action is greater than 3000 than do the action
+      digitalWrite(ledPin, HIGH); 
+      prevMil = curMil; // this tells it to remeber when the last action was. it remebers because the last action occured during the time of this if statement, so it will be close to the same time as this line. 
+    }
+    else if ((unsigned long)(curMil-prevMil > 5000 && switchState == 0)){
+     digitalWrite(ledPin, LOW); 
+      prevMil = curMil;
+    }
+    //this is if the button isnt pressed or if it hasn't reached 3000
+    else {
+      Serial.println("too soon or button not pressed"); 
+      //digitalWrite(ledPin, LOW); 
+    }
+  }
+}
+
+/*void ledON (){
+  for (int i=0; i<6000; i++){
+    digitalWrite(ledPin, HIGH); 
+  }
+}*/
+
---
+++ LumaLegacy_V2/LumaLegacy_V3.ino/LumaLegacy_V3.ino.ino
@@ -0,0 +1,35 @@
+int switchPin = 2; 
+int switchState; 
+int ledPin = 13;
+unsigned long i = 0; 
+unsigned long 
+
+void setup() {
+  Serial.begin(9600); 
+  pinMode(ledPin, OUTPUT);
+  pinMode(switchPin, INPUT);   
+}
+
+void loop() {
+  //tells me if the button is on or off
+  switchState = digitalRead(switchPin); 
+  Serial.println(switchState); 
+  //if the button is on, and if three seconds has passed, then light the LED
+  if (switchState == 0){
+    for(i = 0; i < 10000; i++){
+      Serial.println(i);
+      if (i > 200){
+        digitalWrite(ledPin,HIGH); 
+      } 
+     
+    } 
+  }
+  if (switchState == 0 && i > 400){
+        digitalWrite(ledPin,LOW);
+        Serial.println(switchState);
+        i = 0; 
+  }
+}
+
+
+
